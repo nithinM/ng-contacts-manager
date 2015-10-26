b1a296f514893cb76ee1bb5623d9587c129fdadf
Nithin <nithin@axis.lk>
2015-10-26 16:59:55 +0530

    Initial Commit
    Initial source commit

---
+++ public/src/controller.js
@@ -0,0 +1,15 @@
+angular.module('ContactsApp')
+    .controller('ListController', function ($scope, Contact){
+        $scope.contacts = Contact.query();
+        $scope.fields = ['firstName', 'lastName'];
+
+        //$scope.query = "Something";
+
+        $scope.sort = function (field){
+            $scope.sort.field = field;
+            $scope.sort.order = !$scope.sort.order;
+        };
+
+        $scope.sort.field = 'firstName';
+        $scope.sort.order = false;
+    });
